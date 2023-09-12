0307ec537d2c0beebfb9bd91acf0dbe18d3baaf3
Jordan Mck <ghostdagunna@gmail.com>
2023-09-11 21:49:20 -0500

    Initial Commit

---
+++ Calcuator/CalcuatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcuatorApp.swift
+//  Calcuator
+//
+//  Created by Jordan Mck on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CalcuatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalcuatorUITests/CalcuatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CalcuatorUITestsLaunchTests.swift
+//  CalcuatorUITests
+//
+//  Created by Jordan Mck on 9/11/23.
+//
+
+import XCTest
+
+class CalcuatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
